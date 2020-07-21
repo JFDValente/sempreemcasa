8cce6643ccd0394061246a0a07d63afe895cf6ed
José Valente <jfdv@icomp.ufam.edu.br>
2020-07-20 22:39:38 -0400

    update: configures dependency to style

---
+++ .babelrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    development: {
+      presets: ['next/babel'],
+      plugins: [
+        [
+          'styled-components',
+          {
+            ssr: true,
+            displayName: true,
+            preprocess: false
+          }
+        ]
+      ]
+    },
+    production: {
+      presets: ['next/babel'],
+      plugins: [
+        [
+          'styled-components',
+          {
+            ssr: true,
+            displayName: false,
+            preprocess: false
+          }
+        ]
+      ]
+    }
+  }
+}
